b192477969fd8fd5e570de54b80fe2ee3d3e8c69
s1f102100793 <s1f102100793@iniad.org>
2023-10-15 22:03:29 +0900

    Add Header component

---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from 'react';
+import styles from './Header.module.css';
+
+export const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 800) {
+        setIsMenuOpen(false);
+      }
+    };
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.left}>Spica</div>
+        <div className={styles.right}>
+          <span>ビジョン</span>
+          <span>ニュース</span>
+          <span>お問い合わせ</span>
+          <span>サインイン</span>
+          <span>サインアップ</span>
+        </div>
+        <div
+          className={`${styles.menuButton} ${isMenuOpen ? styles.open : ''}`}
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          <div />
+          <div />
+          <div />
+        </div>
+      </div>
+      <div className={styles.menu}>
+        {isMenuOpen && (
+          <div className={styles.dropdown}>
+            <span className={styles.menuItem} onClick={() => setIsMenuOpen(false)}>
+              ビジョン
+            </span>
+            <span className={styles.menuItem} onClick={() => setIsMenuOpen(false)}>
+              ニュース
+            </span>
+            <span className={styles.menuItem} onClick={() => setIsMenuOpen(false)}>
+              お問い合わせ
+            </span>
+            <span className={styles.menuItem} onClick={() => setIsMenuOpen(false)}>
+              サインイン
+            </span>
+            <span className={styles.menuItem} onClick={() => setIsMenuOpen(false)}>
+              サインアップ
+            </span>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
