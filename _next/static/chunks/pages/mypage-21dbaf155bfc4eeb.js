b1cf8b47b5ae22c2c75c226d7e5481cd0a59801c
s1f102100793 <s1f102100793@users.noreply.github.com>
2023-10-25 01:53:48 +0000

    deploy: fc4166a8870131094fe2c997571c996790c24dd1

---
+++ _next/static/chunks/pages/mypage-21dbaf155bfc4eeb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[587],{7712:function(n,u,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/mypage",function(){return e(1346)}])},1346:function(n,u,e){"use strict";e.r(u);var _=e(5893);u.default=()=>(0,_.jsx)("div",{children:(0,_.jsx)("h1",{children:"MyPage"})})}},function(n){n.O(0,[774,888,179],function(){return n(n.s=7712)}),_N_E=n.O()}]);
