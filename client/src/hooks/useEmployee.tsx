f541d1ba1d00255fe2fc1ab2b64feff507744dcd
s1f102100793 <s1f102100793@iniad.org>
2023-12-16 19:02:08 +0900

    fix:profile page isEditing

--- client/src/hooks/useEmployee.tsx
+++ client/src/hooks/useEmployee.tsx
@@ -8,15 +8,40 @@ import { returnNull } from 'src/utils/returnNull';
 export const useEmployee = () => {
   const [user] = useAtom(userAtom);
   const [employeeInformation, setEmployeeInformation] = useState<EmployeeModel | null>(null);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [profileImage, setProfileImage] = useState('');
 
   const getEmployeeInformation = useCallback(async () => {
-    if (!user) return;
-    await apiClient.employees
+    if (!user) return null;
+    const employeeInformation = await apiClient.employees
       ._employeeId(user.id)
       .$post({})
-      .catch(returnNull)
-      .then(setEmployeeInformation);
-  }, [user, setEmployeeInformation]);
+      .catch(returnNull);
 
-  return { employeeInformation, getEmployeeInformation };
+    if (employeeInformation) {
+      const names = employeeInformation.name.split(' ');
+      setFirstName(names[0]);
+      setLastName(names[1] || '');
+      setEmail(employeeInformation.email);
+      setProfileImage(employeeInformation.profileImage);
+      setEmployeeInformation(employeeInformation);
+    }
+
+    return employeeInformation;
+  }, [user]);
+
+  return {
+    firstName,
+    setFirstName,
+    lastName,
+    setLastName,
+    email,
+    setEmail,
+    profileImage,
+    setProfileImage,
+    employeeInformation,
+    getEmployeeInformation,
+  };
 };
