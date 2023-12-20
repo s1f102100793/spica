a300ebb7967bab212af6174294af4151b58e838f
s1f102100793 <s1f102100793@iniad.org>
2023-12-20 13:31:03 +0900

    fix: gitignore

--- server/api/employee/employeeId/controller.ts
+++
@@ -1,7 +0,0 @@
-import { getEmployee } from '$/repository/employeeRepository';
-import { defineController } from './$relay';
-
-export default defineController(() => ({
-  get: () => ({ status: 200, body: 'Hello' }),
-  post: async ({ body }) => ({ status: 201, body: await getEmployee(body.firebaseUid) }),
-}));
